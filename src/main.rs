47f442041a811889dde055b5639f033418922fa2
jackhodgins <jhodgy44@gmail.com>
2021-03-25 15:01:54 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+use lazy_static::lazy_static;
+use spin::Mutex;
+use pluggable_interrupt_os::HandlerTable;
+use pc_keyboard::DecodedKey;
+use baremetal_snake::SnakeGame;
+use baremetal_snake::MainGame;
+
+lazy_static! {
+    static ref GAME: Mutex<MainGame> = Mutex::new(SnakeGame::new());
+}
+
+fn tick() {
+    baremetal_snake::tick(&mut GAME.lock());
+}
+
+fn key(key: DecodedKey) {
+    GAME.lock().key(key);
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    HandlerTable::new()
+        .keyboard(key)
+        .timer(tick)
+        .start()
+}
